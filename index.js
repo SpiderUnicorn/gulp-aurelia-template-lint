3608290c0b3a9f7f263d5ee0107626b139a38516
Johan Nyström <johan_n694@hotmail.com>
2017-12-06 08:57:47 +0100

    changed error type to html hint

--- index.js
+++ index.js
@@ -5,57 +5,57 @@ var Config = require('aurelia-template-lint').Config;
 var Path = require('path');
 
 
-let issues = [];
 
-module.exports =  {
-    linter: function(config, reporter) {
-        if(!config)  
-           config = new Config();
-      
-        if(!reporter)
-            reporter = (error, file)=>{
-                 gutil.log(`WARNING: ${error.message} Ln ${error.line} Col ${error.column}`, file);
-            }
-              
-        var linter = new AureliaLinter(config);
+module.exports = function(config, reporter) {
+    if(!config)  
+        config = new Config();
     
-        var stream = new Stream.Transform({objectMode: true});
-        stream._transform = function(file, encoding, cb) {
-            // When null just pass through
-            if (file.isNull()) {
-                this.push(file);
-                cb();
-                return;
-            }      
+    if(!reporter)
+        reporter = (error, file)=>{
+                gutil.log(`WARNING: ${error.message} Ln ${error.line} Col ${error.column}`, file);
+        }
             
-            var pathShort = "";
-            
-            if(file.path && file.cwd)
-                pathShort = file.path.substring(file.cwd.length, file.path.Length);
-            
-            var html = String(file.contents);        
-            var self = this;        
-    
-            linter
-                .lint(html, Path.normalize("."+pathShort))    
-                .then((errors) => {
-                    issues = issues.concat(errors)
-                    file.tslint = {
-                        failures: {
-                            errorCount: errors.length,
-                            success: errors.length,
-                            messages: errors
+    var linter = new AureliaLinter(config);
+
+    var stream = new Stream.Transform({objectMode: true});
+    stream._transform = function(file, encoding, cb) {
+        // When null just pass through
+        if (file.isNull()) {
+            this.push(file);
+            cb();
+            return;
+        }      
+        
+        var pathShort = "";
+        
+        if(file.path && file.cwd)
+            pathShort = file.path.substring(file.cwd.length, file.path.Length);
+        
+        var html = String(file.contents);        
+        var self = this;        
+
+        linter
+            .lint(html, Path.normalize("."+pathShort))    
+            .then((errors) => {
+                issues = issues.concat(errors)
+                file.htmlhint = {
+                    messages: errors.map(error => {
+                        return {
+                            severity: 'warning',
+                            line: error.line,
+                            columnNumber: error.column,
+                            raw: error.message,
+                            rule: {
+                                line: undefined
+                            }
                         }
-                    }
-                })
-                .then(() => {
-                    self.push(file);              
-                    cb(null, file) });
-        };
-        return stream;
-    },
-    results: function() {
-        return issues;
-    }
+                    })
+                }
+            })
+            .then(() => {
+                self.push(file);              
+                cb(null, file) });
+    };
+    return stream;
 }
 
